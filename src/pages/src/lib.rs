3b3c23eb9bbb8a5d93195fd00a9a05af831d8fd3
zefr0x <zer0-x.7ty50@aleeas.com>
2025-07-30 15:31:47 +0300

    lint: enable lints in all crates and disable conflicting/problimatic lints

--- src/pages/src/lib.rs
+++ src/pages/src/lib.rs
@@ -1,3 +1,7 @@
+#![expect(clippy::exhaustive_structs, reason = "Leptos component macro problem")]
+#![expect(clippy::must_use_candidate, reason = "Leptos component macro problem")]
+#![expect(clippy::same_name_method, reason = "Leptos component macro problem")]
+
 mod home;
 
 pub use home::*;
