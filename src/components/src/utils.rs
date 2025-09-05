86af48b87d4ecaf13f19dfb9ff4ebe5c8e17bcea
zefr0x <zer0-x.7ty50@aleeas.com>
2025-09-05 18:56:47 +0300

    refac(ui): modularize `ClientCookies` constructor

--- src/components/src/utils.rs
+++ src/components/src/utils.rs
@@ -3,6 +3,18 @@
 use leptos::prelude::*;
 use leptos_use::{use_cookie, use_preferred_dark};
 
+#[derive(Clone, Copy, Debug)]
+struct IsActiveSession {
+	getter: Signal<Option<bool>>,
+	setter: WriteSignal<Option<bool>>,
+}
+
+#[derive(Clone, Copy, Debug)]
+struct IsDarkTheme {
+	getter: Signal<Option<bool>>,
+	setter: WriteSignal<Option<bool>>,
+}
+
 /// Collection of cookies signals that have a dynamic effect in the client
 #[derive(Clone, Copy, Debug)]
 pub struct ClientCookies {
@@ -10,41 +22,38 @@ pub struct ClientCookies {
 	is_dark_theme: IsDarkTheme,
 }
 
-#[derive(Clone, Copy, Debug)]
-struct IsActiveSession {
-	inner: (Signal<Option<bool>>, WriteSignal<Option<bool>>),
+impl IsActiveSession {
+	fn new() -> Self {
+		let (getter, setter) = use_cookie::<bool, codee::string::FromToStringCodec>("is_active_session");
+
+		Self { getter, setter }
+	}
 }
 
-#[derive(Clone, Copy, Debug)]
-struct IsDarkTheme {
-	inner: (Signal<Option<bool>>, WriteSignal<Option<bool>>),
-}
-
-impl ClientCookies {
-	/// Initialize an object with all signals.
-	pub fn new() -> Self {
-		// Dark/Light theme management
-
+impl IsDarkTheme {
+	fn new() -> Self {
 		// To figure out client's preference
 		let is_browser_dark_preferred = use_preferred_dark();
 
-		let (color_theme, set_color_theme) = use_cookie::<bool, codee::string::FromToStringCodec>("is_dark_theme");
+		let (getter, setter) = use_cookie::<bool, codee::string::FromToStringCodec>("is_dark_theme");
 
 		// HACK: Since `Sec-CH-Prefers-Color-Scheme` header is still experimental, we are using cookie.
 		// For the first load, we don't have knowledge about the client's preference in SSR
 		#[expect(unused_results, reason = "No need")]
 		Effect::new(move || {
-			set_color_theme(Some(is_browser_dark_preferred.get()));
+			setter(Some(is_browser_dark_preferred.get()));
 		});
 
-		// Construct client cookies struct
+		Self { getter, setter }
+	}
+}
+
+impl ClientCookies {
+	/// Initialize an object with all signals.
+	pub fn new() -> Self {
 		Self {
-			is_active_session: IsActiveSession {
-				inner: use_cookie::<bool, codee::string::FromToStringCodec>("is_active_session"),
-			},
-			is_dark_theme: IsDarkTheme {
-				inner: (color_theme, set_color_theme),
-			},
+			is_active_session: IsActiveSession::new(),
+			is_dark_theme: IsDarkTheme::new(),
 		}
 	}
 
@@ -52,23 +61,23 @@ impl ClientCookies {
 
 	/// Get `is_active_session` cookie value
 	pub fn is_active_session(self) -> bool {
-		self.is_active_session.inner.0().is_some_and(|x| x)
+		self.is_active_session.getter.get().is_some_and(|x| x)
 	}
 
 	/// Set `is_active_session` cookie value
 	pub fn set_is_active_session(self, value: bool) {
-		self.is_active_session.inner.1(Some(value));
+		self.is_active_session.setter.set(Some(value));
 	}
 
 	/// Get `is_dark_theme` cookie value
 	pub fn is_dark_theme(self) -> bool {
 		// Default to dark theme, since it is less painful for light theme users in the first load
-		self.is_dark_theme.inner.0().unwrap_or(true)
+		self.is_dark_theme.getter.get().unwrap_or(true)
 	}
 
 	/// Set `is_dark_theme` cookie value
 	pub fn set_is_dark_theme(self, value: Option<bool>) {
-		self.is_dark_theme.inner.1(value);
+		self.is_dark_theme.setter.set(value);
 	}
 }
 
